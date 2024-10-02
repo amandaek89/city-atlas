462f9a597892bcedd3539789a81c5779cf5fbb7f
SamerSkay <92samska@gafe.molndal.se>
2024-10-02 14:56:29 +0200

    CityRepo och Model klar

--- src/main/java/com/cityatlas/models/City.java
+++ src/main/java/com/cityatlas/models/City.java
@@ -1,4 +1,78 @@
 package com.cityatlas.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import lombok.Setter;
+import lombok.Getter;
+
+@Setter
+@Getter
+@Entity
 public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private int population;
+    private String knownFor;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    // Constructors, getters, and setters
+    public City() {}
+
+    public City(String name, int population, String knownFor, Country country) {
+        this.name = name;
+        this.population = population;
+        this.knownFor = knownFor;
+        this.country = country;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public String getKnownFor() {
+        return knownFor;
+    }
+
+    public void setKnownFor(String knownFor) {
+        this.knownFor = knownFor;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
 }
