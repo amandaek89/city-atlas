6f09a7e675ab96647030e4d5d868a7998e9d1d2e
Filip <filip.bjorkman1993@gmail.com>
2024-10-03 15:17:09 +0200

    JwtService.java påbörjad

--- src/main/java/com/cityatlas/services/JwtService.java
+++ src/main/java/com/cityatlas/services/JwtService.java
@@ -1,7 +1,48 @@
 package com.cityatlas.services;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.cglib.core.internal.Function;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.HashMap;
 
 @Service
 public class JwtService {
+
+private long jwtExpiration;
+private String secretKey;
+
+public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+    final Claims claims = extractAllClaims(token);
+    return claimsResolver.apply(claims);
+}
+
+private Claims extractAllClaims(String token) {
+    return Jwts.parserBuilder()
+            .setSigningKey(getSigniInKey())
+            .build()
+            .parseClaimsJws(token)
+            .getBody();
+}
+
+public String extractUsername(String token){
+    return extractClaim(token, Claims::getSubject);
+}
+
+private Key getSigniInKey(){
+    byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+    return Keys.hmacShaKeyFor(keyBytes);
+}
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+
+
 }
