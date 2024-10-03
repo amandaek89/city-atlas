bbf71e3b2495dfa64b72369edaca1046a065351a
Isabella <isabella.lindqvist@live.se>
2024-10-03 10:07:19 +0200

    countrymodel

--- src/main/java/com/cityatlas/models/Country.java
+++ src/main/java/com/cityatlas/models/Country.java
@@ -1,4 +1,33 @@
 package com.cityatlas.models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor; //tom konstruktior för att skapa entiteter
+import lombok.AllArgsConstructor;
+import java.util.List;
+
+@Entity
+@Table (name = "countries")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //automatiskt generering av ett id
+    private Long id;
+    private String name;
+    private String language;
+    private Long population;
+
+    //child till världsdel
+    @ManyToOne
+    @JoinColumn (name = "continent_id", nullable = false) //forgein key till världsdel
+    private Continent continent;
+
+    //parent till city
+    //orphanremove så citys till country tas bort vid radering
+    @OneToMany(mappedBy = "country", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<City> cities;
+
+
 }
