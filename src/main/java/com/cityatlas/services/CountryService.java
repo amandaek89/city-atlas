cec51dd1e07f8defe7efe8a94fc90fbcb845ea9e
SamerSkay <92samska@gafe.molndal.se>
2024-10-04 09:51:16 +0200

    fixat krockar

--- src/main/java/com/cityatlas/services/CountryService.java
+++ src/main/java/com/cityatlas/services/CountryService.java
@@ -1,8 +1,5 @@
 package com.cityatlas.services;
 
-<<<<<<<< HEAD:src/main/java/com/cityatlas/services/CountryService.java
 public class CountryService {
-========
-public class CityService {
->>>>>>>> 51f0ddf5063c8c1709bc09116dc07995c969ec3a:src/main/java/com/cityatlas/services/CityService.java
 }
+
