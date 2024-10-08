54f5beafd2da1eb33ceaf45eadbb4986e780279f
Filip <filip.bjorkman1993@gmail.com>
2024-10-08 09:56:32 +0200

    lagt till en admin

---
+++ src/main/java/com/cityatlas/configurations/DataInitializer.java
@@ -0,0 +1,39 @@
+package com.cityatlas.configurations;
+
+import com.cityatlas.models.Role;
+import com.cityatlas.models.User;
+import com.cityatlas.repositories.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class DataInitializer {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private UserRepo userRepo;
+
+    @Bean
+    CommandLineRunner initAdminUser() {
+        return args -> {
+            if (userRepo.findByUsername("admin") == null) {
+                User admin = new User();
+                admin.setUsername("admin");
+                admin.setPassword(passwordEncoder.encode("admin"));
+                Set<Role> roles = new HashSet<>();
+                roles.add(Role.ROLE_ADMIN);
+                admin.setAuthorities(roles);
+                userRepo.save(admin);
+                System.out.println("Admin user created");
+            } else {
+                System.out.println("Admin user already exists");
+            }
+        };
+    }
+}
