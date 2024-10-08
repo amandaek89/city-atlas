ef6cc3ba2ecd120bbf4d08f5c2d434d625728ffb
SamerSkay <92samska@gafe.molndal.se>
2024-10-08 14:54:48 +0200

    fixat med behörigheter för att utföra olika cruds

--- src/main/java/com/cityatlas/controllers/CityController.java
+++ src/main/java/com/cityatlas/controllers/CityController.java
@@ -2,6 +2,7 @@ package com.cityatlas.controllers;
 
 import com.cityatlas.dtos.CityDto;
 import com.cityatlas.services.CityService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,32 +17,35 @@ public class CityController {
         this.cityService = cityService;
     }
 
-    // Hämta alla städer
+    // Hämta alla städer (tillgänglig för både admin och användare)
     @GetMapping
     public List<CityDto> getAllCities() {
         return cityService.getAllCities();
     }
 
-    // Hämta en stad baserat på ID
+    // Hämta en stad baserat på ID (tillgänglig för både admin och användare)
     @GetMapping("/{id}")
     public CityDto getCityById(@PathVariable Long id) {
         return cityService.getCityById(id);
     }
 
-    // Lägg till en ny stad
+    // Lägg till en ny stad (endast för admin)
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public CityDto addCity(@RequestBody CityDto cityDto) {
         return cityService.addCity(cityDto);
     }
 
-    // Uppdatera information om en stad
+    // Uppdatera information om en stad (endast för admin)
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public CityDto updateCity(@PathVariable Long id, @RequestBody CityDto updatedCityDto) {
         return cityService.updateCity(id, updatedCityDto);
     }
 
-    // Ta bort en stad
+    // Ta bort en stad (endast för admin)
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public void deleteCity(@PathVariable Long id) {
         cityService.deleteCity(id);
     }
