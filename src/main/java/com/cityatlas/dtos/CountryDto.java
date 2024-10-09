29d783fe160e1d0238a4818afc5daf3b04fca3b5
Isabella <isabella.lindqvist@live.se>
2024-10-09 10:25:52 +0200

    lagt om dto

--- src/main/java/com/cityatlas/dtos/CountryDto.java
+++ src/main/java/com/cityatlas/dtos/CountryDto.java
@@ -1,16 +1,25 @@
 package com.cityatlas.dtos;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Getter
+@Setter
 public class CountryDto {
-    private Long id;
-    private String name;
-    private String language;
-    private Long population;
-    private Long continentID; //ref till världsdel då Continent är parent
+    private Long id; // Landets ID
+    private String name; // Landets namn
+    private String language; // Språket som talas
+    private Long population; // Antal invånare
+    private Long continentID; // Kopplingen till världsdel, om den finns
+
+    public CountryDto() {} // Standardkonstruktor
+
+    // Konstruktor för att initiera CountryDto med värden
+    public CountryDto(Long id, String name, String language, Long population, Long continentID) {
+        this.id = id;
+        this.name = name;
+        this.language = language;
+        this.population = population;
+        this.continentID = continentID;
+    }
 }
